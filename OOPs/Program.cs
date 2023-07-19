adb73a5ff70e6f9e65c1f1d7d5850d0c539d792a
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-19 18:07:36 +0530

    [jogan] Add : Folder Structure created

---
+++ OOPs/Program.cs
@@ -0,0 +1,38 @@
+﻿using OOPs.DataInventoryManagement;
+using OOPs.InventoryManagement;
+using System;
+namespace OOPs
+{
+    public class Program
+    {
+        static string inventory_filepath = @"E:\Bridgelabz\OOPs\OOPs\OOPs\InventoryManagement\InventoryManagementData.json";
+        private static void Main(string[] args)
+        {
+            //InventoryDetailsOperations details = new InventoryDetailsOperations();
+            //details.ReadInventoryJson(inventory_filepath);
+            bool flag = true;
+            InventoryManagementOperations manage = new InventoryManagementOperations();
+            while (flag)
+            {
+                Console.WriteLine("1.Display inventory\n2.Add to inventory\n3.Exit");
+                int ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        manage.ReadInventoryJson(inventory_filepath);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the item name (Rice,Wheat,Pulse)");
+                        string name = Console.ReadLine();
+                        manage.AddInventoryManagement(name);
+                        manage.WriteToJsonFile(inventory_filepath);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
+            
+        }
+    }
+}
