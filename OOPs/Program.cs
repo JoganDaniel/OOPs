ffaa6ad846ff6e85caae672d4e4531003acdfe57
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-20 12:47:58 +0530

    [jogan] Add : Stock Account Management done

--- OOPs/Program.cs
+++ OOPs/Program.cs
@@ -1,43 +1,45 @@
 ﻿using OOPs.DataInventoryManagement;
 using OOPs.InventoryManagement;
+using OOPs.StockAccountManagement;
 using System;
 namespace OOPs
 {
     public class Program
     {
-        static string inventory_filepath = @"E:\Bridgelabz\OOPs\OOPs\OOPs\InventoryManagement\InventoryManagementData.json";
+        static string stock_filePath = @"E:\Bridgelabz\OOPs\OOPs\OOPs\StockAccountManagement\StockManagementData.json";
         private static void Main(string[] args)
         {
             //InventoryDetailsOperations details = new InventoryDetailsOperations();
             //details.ReadInventoryJson(inventory_filepath);
-            bool flag = true;
-            InventoryManagementOperations manage = new InventoryManagementOperations();
-            while (flag)
-            {
-                Console.WriteLine("1.Display inventory\n2.Add to inventory\n3.Delete from inventory\n4.Exit");
-                int ch = Convert.ToInt32(Console.ReadLine());
-                switch (ch)
-                {
-                    case 1:
-                        manage.ReadInventoryJson(inventory_filepath);
-                        break;
-                    case 2:
-                        Console.WriteLine("Enter the item name (Rice,Wheat,Pulse)");
-                        string name = Console.ReadLine();
-                        manage.AddInventoryManagement(name);
-                        manage.WriteToJsonFile(inventory_filepath);
-                        break;
-                    case 3:
-                        Console.WriteLine("Enter the item name (Rice,Wheat,Pulse)");
-                        string name1 = Console.ReadLine();
-                        manage.DeleteInventory(name1, inventory_filepath);
-                        break;
-                    case 4:
-                        flag = false;
-                        break;
-                }
-            }
-            
+            //bool flag = true;
+            //InventoryManagementOperations manage = new InventoryManagementOperations();
+            //while (flag)
+            //{
+            //    Console.WriteLine("1.Display inventory\n2.Add to inventory\n3.Delete from inventory\n4.Exit");
+            //    int ch = Convert.ToInt32(Console.ReadLine());
+            //    switch (ch)
+            //    {
+            //        case 1:
+            //            manage.ReadInventoryJson(inventory_filepath);
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("Enter the item name (Rice,Wheat,Pulse)");
+            //            string name = Console.ReadLine();
+            //            manage.AddInventoryManagement(name);
+            //            manage.WriteToJsonFile(inventory_filepath);
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("Enter the item name (Rice,Wheat,Pulse)");
+            //            string name1 = Console.ReadLine();
+            //            manage.DeleteInventory(name1, inventory_filepath);
+            //            break;
+            //        case 4:
+            //            flag = false;
+            //            break;
+            //    }
+            //}
+            StockOperations operations = new StockOperations();
+            operations.ReadStockJson(stock_filePath);
         }
     }
 }
---
+++ OOPs/StockAccountManagement/StockOperations.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using OOPs.DataInventoryManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs.StockAccountManagement
+{
+    internal class StockOperations
+    {
+        public void ReadStockJson(string filepath)
+        {
+            var json = File.ReadAllText(filepath);
+            List<StockDetails> list = JsonConvert.DeserializeObject<List<StockDetails>>(json);
+            foreach (var data in list)
+            {
+                Console.WriteLine(data.StockName + " " + data.NoOfShares + " " + data.SharePrice);
+
+            }
+        }
+}
+}
